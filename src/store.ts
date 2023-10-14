5cc877f8c972a9858cef7b1a9a4f8211c08c4d3e
Laurent Mbuyu <LMbuyu@osiristrading.com>
2023-10-14 11:20:20 +0200

    [New]: Reservation has been added to the store.

--- src/store.ts
+++ src/store.ts
@@ -4,6 +4,7 @@ type State = {
     name: string;
     place: any[];
     event: any;
+    reservation: any;
     chosenPlace: string;
     chosenReservationType: string;
 }
@@ -12,6 +13,7 @@ type Action = {
     setName: (name: string) => void;
     setPlace: (place: any[]) => void;
     setEvent: (event: any) => void;
+    setReservation: (reservation: any) => void;
     setChosenPlace: (chosenPlace: string) => void;
     setReservationType: (event: State['chosenReservationType']) => void;
 }
@@ -20,11 +22,13 @@ export const useStore = create<State & Action>((set) => ({
     name: "",
     place: [],
     event: null,
+    reservation: null,
     chosenPlace: "",
     chosenReservationType: "",
     setName: (name) => set(() => ({ name: name })),
     setPlace: (place) => set({ place: place }),
     setEvent: (event) => set({ event: event }),
+    setReservation: (reservation) => set({ reservation: reservation }),
     setChosenPlace: (chosenPlace) => set({ chosenPlace: chosenPlace }),
     setReservationType: (chosenReservationType) => set({ chosenReservationType: chosenReservationType }),
 }));
